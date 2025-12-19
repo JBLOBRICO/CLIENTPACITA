46c8b013c7ce1555efd082b84a366ce178c93560
JBLOBRICO <C2903-23@itmlyceumalabang.onmicrosoft.com>
2025-12-19 20:17:42 +0800

    sdsaiud

--- ClientModule.vb
+++ ClientModule.vb
@@ -3,6 +3,7 @@ Imports System.Text
 Imports System.Threading
 
 Module ClientModule
+
     Public client As TcpClient
     Private clientStream As NetworkStream
     Private reader As IO.StreamReader
@@ -11,20 +12,29 @@ Module ClientModule
 
     Public serverIP As String = "127.0.0.1"
     Public serverPort As Integer = 5000
+
     Public isConnected As Boolean = False
 
-    ' Connect to server
+    ' 🔑 PC ID (important!)
+    Public PCID As String = Environment.MachineName
+
+    ' ==========================
+    ' CONNECT TO SERVER
+    ' ==========================
     Public Sub ConnectToServer()
         Try
             client = New TcpClient()
             client.Connect(serverIP, serverPort)
-            clientStream = client.GetStream()
 
+            clientStream = client.GetStream()
             reader = New IO.StreamReader(clientStream, Encoding.UTF8)
             writer = New IO.StreamWriter(clientStream, Encoding.UTF8) With {.AutoFlush = True}
 
             isConnected = True
             Console.WriteLine("Connected to server.")
+
+            ' 🔑 REGISTER PC
+            writer.WriteLine("REGISTER|" & PCID)
 
             receiveThread = New Thread(AddressOf ReceiveData)
             receiveThread.IsBackground = True
@@ -36,7 +46,9 @@ Module ClientModule
         End Try
     End Sub
 
-    ' Send message to server
+    ' ==========================
+    ' SEND MESSAGE
+    ' ==========================
     Public Sub SendMessage(message As String)
         If isConnected Then
             Try
@@ -47,33 +59,39 @@ Module ClientModule
         End If
     End Sub
 
-    ' Receive loop
+    ' ==========================
+    ' RECEIVE LOOP
+    ' ==========================
     Private Sub ReceiveData()
         Try
             While isConnected
                 Dim msg As String = reader.ReadLine()
-                If msg Is Nothing Then
-                    isConnected = False
-                    Exit While
-                End If
+                If msg Is Nothing Then Exit While
                 ProcessServerCommand(msg)
             End While
         Catch ex As Exception
-            Console.WriteLine("Disconnected from server: " & ex.Message)
+            Console.WriteLine("Disconnected: " & ex.Message)
+        Finally
             isConnected = False
         End Try
     End Sub
 
-    ' Handle commands from server
+    ' ==========================
+    ' PROCESS SERVER COMMANDS
+    ' ==========================
     Private Sub ProcessServerCommand(msg As String)
         Dim parts() As String = msg.Split("|"c)
+
         Select Case parts(0).ToUpper()
+
             Case "START_SESSION"
                 ' START_SESSION|PCID|SessionType|Amount
+                If parts(1) <> PCID Then Exit Sub
+
                 Dim amount As Decimal = Decimal.Parse(parts(3))
                 CLIENTDASHBOARD.Invoke(Sub()
                                            CLIENTDASHBOARD.Hide()
-                                           CLIENTSESSION.SetPCName(Environment.MachineName)
+                                           CLIENTSESSION.SetPCName(PCID)
                                            CLIENTSESSION.SetAmount(amount)
                                            CLIENTSESSION.SetTime("00:00:00")
                                            CLIENTSESSION.Show()
@@ -82,26 +100,13 @@ Module ClientModule
 
             Case "END_SESSION"
                 ' END_SESSION|PCID
+                If parts(1) <> PCID Then Exit Sub
+
                 CLIENTSESSION.Invoke(Sub()
                                          CLIENTSESSION.StopSessionTimer()
                                          CLIENTSESSION.Hide()
                                          CLIENTDASHBOARD.Show()
                                      End Sub)
-
-            Case "SHUTDOWN"
-                ' SHUTDOWN|PCID
-                CLIENTDASHBOARD.Invoke(Sub()
-                                           MessageBox.Show("PC will shutdown in 10 seconds.", "Shutdown", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-                                       End Sub)
-                Thread.Sleep(10000)
-                Process.Start("shutdown", "/s /t 0")
-
-            Case "SEND_MESSAGE"
-                ' SEND_MESSAGE|Text
-                Dim messageText As String = parts(1)
-                CLIENTDASHBOARD.Invoke(Sub()
-                                           CLIENTDASHBOARD.SetMessage(messageText)
-                                       End Sub)
 
             Case "UPDATE_AMOUNT"
                 ' UPDATE_AMOUNT|Amount
@@ -110,22 +115,41 @@ Module ClientModule
                                          CLIENTSESSION.SetAmount(amount)
                                      End Sub)
 
+            Case "SEND_MESSAGE"
+                ' SEND_MESSAGE|Text
+                CLIENTDASHBOARD.Invoke(Sub()
+                                           CLIENTDASHBOARD.SetMessage(parts(1))
+                                       End Sub)
+
+            Case "SHUTDOWN"
+                ' SHUTDOWN|PCID
+                If parts(1) <> PCID Then Exit Sub
+
+                CLIENTDASHBOARD.Invoke(Sub()
+                                           MessageBox.Show("PC will shutdown in 10 seconds.", "Shutdown",
+                                                           MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                                       End Sub)
+
+                Thread.Sleep(10000)
+                Process.Start("shutdown", "/s /t 0")
+
             Case Else
-                Console.WriteLine("Unknown server command: " & msg)
+                Console.WriteLine("Unknown command: " & msg)
         End Select
     End Sub
 
-    ' Disconnect cleanly
+    ' ==========================
+    ' DISCONNECT CLEANLY
+    ' ==========================
     Public Sub DisconnectClient()
         Try
-            If isConnected Then
-                reader.Close()
-                writer.Close()
-                clientStream.Close()
-                client.Close()
-                isConnected = False
-            End If
-        Catch ex As Exception
+            isConnected = False
+            reader?.Close()
+            writer?.Close()
+            clientStream?.Close()
+            client?.Close()
+        Catch
         End Try
     End Sub
+
 End Module
